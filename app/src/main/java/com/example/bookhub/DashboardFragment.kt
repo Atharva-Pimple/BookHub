ed1e828819140aead818d1a713de1618bda55da0
Atharva Pimple <atharvapimple30@gmail.com>
2023-09-24 12:13:33 +0530

    Recycler Adaptor created

--- app/src/main/java/com/example/bookhub/DashboardFragment.kt
+++ app/src/main/java/com/example/bookhub/DashboardFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class DashboardFragment : Fragment() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view=inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        recyclerView=view.findViewById(R.id.recyclerDashboard)
+        layoutManager=LinearLayoutManager(activity)
+
+
         return view
     }
 
